61a7d79293467f86c9fdb0f8ca04a3f376f104d2
moazkhan <moazkh60@yahoo.com>
2016-01-03 21:52:07 +0500

    With Login
    Integrated with Umar

---
+++ conection/connect.php
@@ -0,0 +1,14 @@
+<?php
+<<<<<<< HEAD
+	mysql_connect("localhost","root","") or die("No Connection");
+=======
+	mysql_connect("localhost","root","seecs@123") or die("No Connection");
+>>>>>>> origin/master
+	mysql_select_db("database_new") or die("No Database name");
+?>
+
+
+<!-- $mysql_host = "mysql2.000webhost.com";
+$mysql_database = "a4451455_school1";
+$mysql_user = "a4451455_jazib";
+$mysql_password = "jazib123"; -->
---
+++ logout.php
@@ -0,0 +1,10 @@
+<?php
+	session_start();
+	session_destroy();
+<<<<<<< HEAD
+	header('Location: ../../portal.php');
+=======
+	header('Location: index.php');
+>>>>>>> origin/master
+	exit; 
+?>
